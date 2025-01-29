5251b38780052cbde264030c460b4578cb9a4560
toby <tobygeneva@hotmail.com>
2025-01-29 13:35:22 +0100

    Initial Commit

---
+++ emi2/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  emi2
+//
+//  Created by Toby Simpson on 29.01.2025.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
